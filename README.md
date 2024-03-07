dd2de4e689c6f2ef6785f6255d5fd906a2cdfa7f
newerk <juniusowens@yahoo.com>
2024-03-07 16:30:19 -0500

    Remove Unused Boiler Plate Files and Code from Express-Geneerator

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # mini-message-board
 
 A small interactive message board app that showcases some of the things I learned while getting familiar with the Express framework
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const messages = require("./messages");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res) => {
+  await res.render("index", { messages: messages });
 });
 
 module.exports = router;
