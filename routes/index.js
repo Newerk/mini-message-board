d5439e49a5a38ae57134db73cd71621cf1136538
newerk <juniusowens@yahoo.com>
2024-03-07 17:21:55 -0500

    Add Post Req to Add New Message to Board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const messages = require("./messages");
+const bodyParser = require("body-parser");
+
 const router = express.Router();
+
+router.use(bodyParser.urlencoded());
+router.use(bodyParser.json());
 
 router.get("/", async (req, res) => {
   await res.render("index", { messages: messages });
+});
+
+router.get("/new", async (req, res) => {
+  await res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  const author = req.body.author;
+  const msgText = req.body.msg;
+
+  bodyParser.urlencoded({ extended: true });
+
+  messages.push({ user: author, text: msgText, added: new Date() });
+
+  res.redirect("/");
 });
 
 module.exports = router;
