66130e8d74b28d61257a65f78c4732fe007c6b21
newerk <juniusowens@yahoo.com>
2024-03-11 15:25:48 -0400

    Remove PORT Variable That Was in app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const IndexRouter = require("./routes/index");
-const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
